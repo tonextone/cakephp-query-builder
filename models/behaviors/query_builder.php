7d0e7dec951a513b913dcc7b89420e120a8b3062
Takayuki Miwa <i@tkyk.name>
2010-07-23 21:25:52 +0900

    Added getQueryOptions method and updated finder method to use it

--- models/behaviors/query_builder.php
+++ models/behaviors/query_builder.php
@@ -3,7 +3,7 @@
  * QueryBuilder Behavior
  * 
  * <code>
- * $model->finder('all')
+ * $Model->finder('all')
  *   ->fields('id', 'title')
  *   ->Model_group_id(100)
  *   ->Model_title('LIKE', 'var')
@@ -25,13 +25,55 @@ App::import('Lib', 'QueryBuilder.QueryOptions');
 class QueryBuilderBehavior extends ModelBehavior {
 
     /**
+     * @var array
+     */
+    public $errorTemplate = array('code' => 500,
+                                  'name' => 'Missing Query Options');
+
+    /**
+     * This method is public but not available through the Models.
+     * 
+     * @param string  config name
+     */
+    public function _missingOptionsError($name) {
+        return $this->errorTemplate + array('message' => $name);
+    }
+
+    /**
+     * Returns a query options array named $name
+     * 
+     * @param string  option name
+     * @return array
+     */
+    public function getQueryOptions($model, $name) {
+        if(!isset($model->queryOptions) ||
+           !isset($model->queryOptions[$name]) ||
+           !is_array($model->queryOptions[$name])) {
+            $model->cakeError('error',
+                              $this->_missingOptionsError($name));
+            return;
+        }
+        return $model->queryOptions[$name];
+    }
+
+    /**
+     * <code>
+     * $finder = $Model->finder('all')->fields(...)->conditions(...);
+     * 
+     * // using query options
+     * $finder = $Model->finder('all', 'approved', 'normal_order')->limit(...);
+     * </code>
      * 
      * @param object  Model
      * @param string  find type
      * @return QueryMethod
      */
     public function finder($model, $type) {
-        $finder = $this->createQueryMethod($model, 'find', array($type));
+        $finder = $model->createQueryMethod('find', array($type));
+        if(func_num_args() > 2) {
+            $args = func_get_args();
+            $finder->import(array_slice($args, 2));
+        }
         return $finder;
     }
 
--- tests/cases/behaviors/query_builder.test.php
+++ tests/cases/behaviors/query_builder.test.php
@@ -1,6 +1,19 @@
 <?php
 
 App::import('Behavior', 'QueryBuilder.QueryBuilder');
+Mock::generate('Model', 'MockActsAsQueryBuilder', array('createQueryMethod'));
+Mock::generate('QueryMethod');
+
+class TestModelForQueryBuilderTestCase extends Model {
+    var $useTable = false;
+    var $actsAs = array('QueryBuilder.QueryBuilder');
+    var $queryOptions
+        = array('default' => array('limit' => 10),
+                'more' => array('limit' => 100),
+                'normal_order' => array('order' => 'created ASC'),
+                'approved' => array('conditions'
+                                    => array('status' => 'approved')));
+}
 
 class QueryBuilderTestCase extends CakeTestCase {
     var $f;
@@ -16,6 +29,39 @@ class QueryBuilderTestCase extends CakeTestCase {
     function testInit() {
     }
 
+    function testGetQueryOptions() {
+        $model = new stdClass;
+        $model->queryOptions
+            = array('default' => array('fields' => a('id', 'title'),
+                                       'limit' => 50),
+                    'approved' => array('conditions'
+                                        => array('status' => 'approved')));
+
+        foreach($model->queryOptions as $name => $arr) {
+            $this->assertEqual($arr,
+                               $this->f->getQueryOptions($model, $name));
+        }
+    }
+
+    function testGetQueryOptions_Error() {
+        $key = 'no_such_key';
+
+        $expectedError = $this->f->_missingOptionsError($key);
+
+        $model = new MockActsAsQueryBuilder;
+        $model->expectCallCount('cakeError', 3);
+        $model->expect('cakeError', array('error', $expectedError));
+
+        $this->assertNull($this->f->getQueryOptions($model, $key));
+
+        $model->queryOptions = array('foo' => 'bar');
+        $this->assertNull($this->f->getQueryOptions($model, $key));
+
+        // found but not an array
+        $model->queryOptions = array($key => null);
+        $this->assertNull($this->f->getQueryOptions($model, $key));
+    }
+
     function testCreateQueryMethod() {
         $model = new stdClass;
         $method = 'find';
@@ -29,15 +75,69 @@ class QueryBuilderTestCase extends CakeTestCase {
     }
 
     function testFinder() {
-        $model = new stdClass;
         $type = 'all';
+        $returnObj = new stdClass;
+
+        $model = new MockActsAsQueryBuilder;
+        $model->expectOnce('createQueryMethod',
+                           array('find', array($type)));
+        $model->setReturnValue('createQueryMethod', $returnObj);
 
         $finder = $this->f->finder($model, $type);
-        $this->assertIsA($finder, 'QueryMethod');
-        $this->assertIdentical('find', $finder->getMethod());
-        $this->assertIdentical($model, $finder->getTarget());
-        $this->assertIdentical(array($type), $finder->getAllArguments());
+        $this->assertIdentical($returnObj, $finder);
+    }
 
+    function testFinder_QueryOptions() {
+        $type = 'all';
+
+        $returnObj = new MockQueryMethod;
+        $model = new MockActsAsQueryBuilder;
+
+        // setup Mocks
+        $model->queryOptions
+            = array('common' => array('limit' => 50,
+                                      'order' => 'id DESC',
+                                      'conditions' => 'id NOT NULL'),
+                    'approved' => array('conditions' => array('status' => 'approved'),
+                                        'limit' => 100));
+        $model->expectOnce('createQueryMethod',
+                           array('find', array($type)));
+        $model->setReturnValue('createQueryMethod', $returnObj);
+
+        $returnObj->expectOnce('import', array(array_keys($model->queryOptions)));
+
+
+        $finder = $this->f->finder($model, $type, 'common', 'approved');
+        $this->assertIdentical($returnObj, $finder);
+    }
+
+    function testFinder_Attached() {
+        $model = new TestModelForQueryBuilderTestCase();
+
+        $f = $model->finder('all')
+            ->fields('id', 'title')
+            ->order('id ASC')
+            ->User_id(3)
+            ->User_created('>', '2010-01-01')
+            ->limit(20);
+
+        $this->assertIsA($f, 'QueryMethod');
+        $this->assertIdentical(array('all'), $f->args);
+        $this->assertIdentical(array('id', 'title'), $f->fields);
+        $this->assertIdentical('id ASC', $f->order);
+        $this->assertIdentical(array('User.id' => 3,
+                                     'User.created >' => '2010-01-01'),
+                               $f->conditions);
+        $this->assertIdentical(20, $f->limit);
+
+        $f2 = $model->finder('first', 'more', 'approved')
+            ->conditions('title IS NOT NULL');
+        $this->assertIsA($f2, 'QueryMethod');
+        $this->assertIdentical(array('status' => 'approved',
+                                     'title IS NOT NULL'),
+                               $f2->conditions);
+        $this->assertIdentical(100, $f2->limit);
+        
     }
 
 }
